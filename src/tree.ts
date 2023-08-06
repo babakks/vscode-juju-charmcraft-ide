2e3da16264f52058d88b70485dafaee3b4767738
Babak K. Shandiz <babak.k.shandiz@gmail.com>
2023-08-06 13:43:12 +0100

    Remove activity bar entry
    Signed-off-by: Babak K. Shandiz <babak.k.shandiz@gmail.com>

--- src/tree.ts
+++
@@ -1,9 +0,0 @@
-import { Disposable } from "vscode";
-
-export class ExtensionTree implements Disposable {
-    private readonly _disposables: Disposable[] = [];
-
-    dispose() {
-        this._disposables.forEach(x => x.dispose());
-    }
-}
