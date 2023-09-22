20f3c4f510f8f84f0f715e8d45405489096bc49b
Babak K. Shandiz <babak.k.shandiz@gmail.com>
2023-09-22 13:42:32 +0100

    ➕ Add `CharmToxConfig` and related types
    Signed-off-by: Babak K. Shandiz <babak.k.shandiz@gmail.com>

--- src/model/charm.ts
+++ src/model/charm.ts
@@ -261,6 +261,23 @@ export interface CharmActions {
     node: YAMLNode;
 }
 
+/*
+ * TODO
+ * We have skipped AST-scope granularity (e.g., nodes or problems) for tox
+ * configuration model, for now. Maybe at some point in future we decided to add
+ * them to the type, after which point the type should look like others (e.g.,
+ * actions or config ) where WithNode<T> has replaced primitive types.
+ */
+export interface CharmToxConfig {
+    sections: { [key: string]: CharmToxConfigSection };
+}
+
+export interface CharmToxConfigSection {
+    name: string;
+    description?: string;
+    commands?: string;
+}
+
 export class CharmSourceCodeFile {
     private _analyzer: CharmSourceCodeFileAnalyzer | undefined;
     private _tpm: TextPositionMapper | undefined;
