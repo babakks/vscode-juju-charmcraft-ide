e3a9240679b478cefc239d8922f05f738f1c4a06
Babak K. Shandiz <babak.k.shandiz@gmail.com>
2023-11-07 06:01:32 +0000

    🔨 Install tox environment requirements
    Signed-off-by: Babak K. Shandiz <babak.k.shandiz@gmail.com>

--- resource/venv/setup.sh
+++ resource/venv/setup.sh
@@ -14,4 +14,14 @@ pip3 install \
     juju \
     pytest-operator
 find . -maxdepth 1 -type f -name 'requirements*.txt' -exec pip3 install -r {} \;
+
+# Setting up tox evnironments.
+# (this only works with bash.)
+IFS="," read -r -a envs <<< "$(tox list --no-desc | sed -z -e 's/\n/,/g' -e 's/,$//')"
+for e in "${envs[@]}"
+do
+    fullname=$(tox config -e "$e" | head -n1 | grep -P --only-matching '(?<=\[).*(?=\])')
+    tox exec -e "$e" -x "$fullname.allowlist_externals=sh" -- sh -c echo "Dependencies installed: $e"
+done
+
 cd -
